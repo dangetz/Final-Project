caa4dc7fe9315a385239f37abbf1caa9aabc99ab
shreya857 <shsnair@iu.edu>
2018-10-25 16:16:20 -0400

    remove post

--- public/js/sites.app.js
+++ public/js/sites.app.js
@@ -2,45 +2,14 @@ var siteApp = new Vue({
   el: '#siteVue',
   data: {
     site: [ ],
-    siteForm: { },
+
   },
 
-  methods: {
-    handleSiteForm(e) {
-      e.preventDefault();
-
-      const s = JSON.stringify(this.siteForm);
-
-      fetch('api/site.php', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        body: s
-      })
-      .then( response => response.json() )
-      .then( json => {this.site.push(json)})
-      .catch( err => {
-        console.error('POST ERROR:');
-        console.error(err);
-      })
-
-      this.siteForm = this.getEmptySitesForm();
-    },
-
-    getEmptySitesForm() {
-      return {
-        comment: null
-        }
-      },
-    },
 
 
   created() {
 
-    this.siteForm = this.getEmptySitesForm();
 
-    console.log('IS THIS BEING CALLED');
 
     fetch('api/site.php')
     .then( response => response.json() )
