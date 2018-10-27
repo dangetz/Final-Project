277dcc1f6f614397a8b9eac4b0c5cd965befaf55
Dan Getz <dangetz@indiana.edu>
2018-10-26 21:51:48 -0400

    added kpi.app sites

---
+++ public/js/kpi.app.js
@@ -0,0 +1,27 @@
+var siteApp = new Vue({
+  el: '#siteVue',
+  data: {
+    site: [ ],
+
+  },
+
+
+
+  created() {
+
+
+
+    fetch('api/site.php')
+    .then( response => response.json() )
+    .then( json => {siteApp.site = json} )
+    .catch( err => {
+      console.log('ERROR WITH FETCH');
+      console.log(err);
+    })
+  },
+  methods: {
+  goToSite(sid){
+    window.location ='turbinesDeployed.html?siteId=' + sid;
+  }
+}}
+)
