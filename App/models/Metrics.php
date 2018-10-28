6d4ba60edf06c0f21de6f64cae4dc1318c634a9b
Zach Beeler <zbeeler@iu.edu>
2018-10-28 15:08:58 -0400

    Updating

--- App/models/Metrics.php
+++ App/models/Metrics.php
@@ -1,20 +1,38 @@
 <?php
 
 class Metrics {
-  public $turbineId;
-  public $capacity;
-  public $rampUpTime;
-  public $maintenanceInterval;
-  public $totalStarts;
-  public $totalFiredHours;
+  public $turbineDeployedId;
+  public $turbineName;
+  public $turbineSerialNumber;
+  public $sensorName;
+  public $sensorSerialNumber;
+  public $sensorDeployedId;
+  public $dataCollectedDate;
+  public $output;
+  public $heatRate;
+  public $compressorEfficiency;
+  public $availability;
+  public $reliability;
+  public $firedHours;
+  public $trips;
+  public $starts;
 
   public function __construct($data) {
-    $this ->turbineId = $data['turbineId'];
-    $this ->capacity = $data['capacity'];
-    $this ->rampUpTime = $data['rampUpTime'];
-    $this ->maintenanceInterval = $data['maintenanceInterval'];
-    $this ->totalStarts = $data['totalStarts'];
-    $this ->totalFiredHours = $data['totalFiredHours'];
+    $this ->turbineDeployedId = $data['turbineDeployedId'];
+    $this ->turbineName = $data['turbineName'];
+    $this ->turbineSerialNumber = $data['turbineSerialNumber'];
+    $this ->sensorName = $data['sensorName'];
+    $this ->sensorSerialNumber = $data['sensorSerialNumber'];
+    $this ->sensorDeployedId = $data['sensorDeployedId'];
+    $this ->dataCollectedDate = $data['dataCollectedDate'];
+    $this ->output = $data['output'];
+    $this ->heatRate = $data['heatRate'];
+    $this ->compressorEfficiency = $data['compressorEfficiency'];
+    $this ->availability = $data['availability'];
+    $this ->reliability = $data['reliability'];
+    $this ->firedHours = $data['firedHours'];
+    $this ->trips = $data['trips'];
+    $this ->starts = $data['starts'];
   }
 
   public static function fetchMetrics() {
