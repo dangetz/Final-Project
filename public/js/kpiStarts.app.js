5dbd01d338280492f45b90026ae3930021d62e98
Zach Beeler <zbeeler@iu.edu>
2018-10-29 14:29:42 -0400

    changing parse

--- public/js/kpiStarts.app.js
+++ public/js/kpiStarts.app.js
@@ -24,7 +24,7 @@ var kpiStartsApp = new Vue({
     formatSensorTime(){
       this.sensorTime.forEach(
         (entry, index, arr) => {
-          entry.starts = parseInt(entry.starts);
+          entry.starts = parseFloat(entry.starts);
           entry.dateCollected = Date.parse(entry.dataCollectedDate);
         }
       )
